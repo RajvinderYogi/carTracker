42880ba0ab187dc5e037bc66103fa179aa8eefae
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-02-13 10:37:31 -0500

    first commit with navigation

---
+++ controllers/index.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Car Tracker' , message: 'COMP 2106 In-Class Node Application' });
+});
+//GET:/ about
+
+router.get('/about',(req, res, next) => {
+  //load about view
+    res.render('about', {
+      title: 'About the car tracker',
+        message: 'This app is built with MEAN stack'
+    });
+});
+
+//GET: /contact
+router.get('/contact',(req, res, next) => {
+    //load about view
+    res.render('contact', {
+        title: 'Contact Us',
+        message: 'Here is the contact Information'
+    });
+});
+module.exports = router;
